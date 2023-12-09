c2fea045b1d0690011eaa6ab4962a71faedc977d
Mario <mario.dias3100@gmail.com>
2023-12-09 19:22:11 +0530

    Day 7

---
+++ Day 7/part2.py
@@ -0,0 +1,72 @@
+from collections import Counter
+import functools
+
+file = open("input.txt", "r")
+lines = file.read().split(sep="\n")
+
+card_values = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J']
+
+
+def determine_strength(x):
+    joker_count = Counter(x).get('J', 0)
+    x_counter = Counter(x.replace('J', '')).most_common()
+    if (joker_count == 5
+            or x_counter[0][1] == 5
+            or (x_counter[0][1] == 4 and joker_count == 1)
+            or (x_counter[0][1] == 3 and joker_count == 2)
+            or (x_counter[0][1] == 2 and joker_count == 3)
+            or (x_counter[0][1] == 1 and joker_count == 4)):
+        return 1
+    elif (x_counter[0][1] == 4
+          or (x_counter[0][1] == 3 and joker_count == 1)
+          or (x_counter[0][1] == 2 and joker_count == 2)
+          or (x_counter[0][1] == 1 and joker_count == 3)):
+        return 2
+    elif ((x_counter[0][1] == 3 and x_counter[1][1] == 2)
+          or (x_counter[0][1] == 2 and x_counter[1][1] == 2 and joker_count == 1)):
+        return 3
+    elif (x_counter[0][1] == 3
+          or (x_counter[0][1] == 2 and joker_count == 1)
+          or (x_counter[0][1] == 1 and joker_count == 2)):
+        return 4
+    elif x_counter[0][1] == 2 and x_counter[1][1] == 2:
+        return 5
+    elif (x_counter[0][1] == 2
+          or (x_counter[0][1] == 1 and joker_count == 1)):
+        return 6
+    else:
+        return 7
+
+
+def compare_cards(x, y):
+    for i in range(5):
+        if x[i] != y[i]:
+            return card_values.index(x[i]) - card_values.index(y[i])
+    return 0
+
+
+def compare(x, y):
+    x_strength = determine_strength(x)
+    y_strength = determine_strength(y)
+
+    if x_strength == y_strength:
+        return compare_cards(x, y)
+    else:
+        return x_strength - y_strength
+
+
+hands = []
+hands_bid_map = dict()
+
+for line in lines:
+    hand, bid = line.split()
+    hands.append(hand)
+    hands_bid_map[hand] = int(bid)
+
+hands.sort(key=functools.cmp_to_key(compare))
+hands.reverse()
+print(hands)
+
+output = sum([idx * hands_bid_map[hand] for idx, hand in enumerate(hands, start=1)])
+
+print(output)
